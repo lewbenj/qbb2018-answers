54cec88441f68ef5e2d6e3ee9e0637446e3e5b8c
lewbenj <lewis_bennett@live.com>
2018-08-28 14:16:10 -0400

    day2 exercise1_2

---
+++ day2-lunch/day2-exercise-1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys 
+
+if len(sys.argv) > 1:
+    f = open( sys.argv[1])
+else:
+    f = sys.stdin
+
+count = 0 
+for line in f:
+    if line.startswith("@"):
+        continue
+        count += 1 
+print(count)
+
+ 
+
+    
